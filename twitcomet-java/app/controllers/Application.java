300a1eba1bcc0db64df2a8067a1fe148460493d2
Julien Lafont <yotsumi.fx@gmail.com>
2012-03-03 22:55:59 +0100

    Correction import manquant sur Application.java

--- twitcomet-java/app/controllers/Application.java
+++ twitcomet-java/app/controllers/Application.java
@@ -5,13 +5,13 @@ import java.util.List;
 import models.Message;
 import play.mvc.Controller;
 import play.mvc.Result;
+import views.html.*;
 
 public class Application extends Controller {
   
   public static Result index() {
 	List<Message> messages = Message.findGlobalLastMessages(40l);
 	Long lastId = messages.get(messages.size()-1).id;
-	
 	
     return ok(index.render(messages, lastId));
   }
